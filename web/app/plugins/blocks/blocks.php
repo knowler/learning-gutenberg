07332d6cf4265c8e5005320d46377fc06f09f064
Nathan Knowler <nathan@knowler.me>
2019-03-06 00:22:27 -0700

    Check if blocks config exists first

--- web/app/plugins/blocks/blocks.php
+++ web/app/plugins/blocks/blocks.php
@@ -11,9 +11,11 @@
 !file_exists($composer = __DIR__ . '/vendor/autoload.php') || require_once $composer;
 
 add_action('init', function () {
-    $blocks = json_decode(file_get_contents(__DIR__ . '/resources/blocks.json'));
+    if (file_exists($blocksConfigPath = __DIR__ . '/resources/blocks.json')) {
+        $blocks = (array) json_decode(file_get_contents($blocksConfigPath));
 
-    foreach ((array) $blocks as $block => $filepath) {
-        new Knowler\Blocks\Block($block);
+        foreach ($blocks as $block => $filepath) {
+            new Knowler\Blocks\Block($block);
+        }
     }
 });
